cd0dd4c091ef5c0c5268beb5f64bea2850d459dc
Beverly Potts <pottsbeverly@gmail.com>
2020-06-09 13:51:50 -0400

    Create function to accept initial grocery array and send each item through a loop that capitalizes the first letter, adds each item to a new array, and returns it as a sorted array

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,9 @@
-$(document).ready(function() {
-
-});
+// Business Logic
+function transformGroceryList(arrayParameter) {
+  let newArray = [];
+  arrayParameter.forEach(function(groceryItem) {
+   groceryItem = groceryItem.charAt(0).toUpperCase() + groceryItem.slice(1);
+   newArray.push(groceryItem);
+  })
+  return newArray.sort();
+}

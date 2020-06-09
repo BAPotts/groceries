b2c93a2b2958ffee31ff16d6ea1aaf89384e0204
Beverly Potts <pottsbeverly@gmail.com>
2020-06-09 14:34:41 -0400

    Create transformedList array to catch return of transformGroceryList function; make forEach loop to add each iteration of transformedList array to unordered list

--- js/scripts.js
+++ js/scripts.js
@@ -2,8 +2,32 @@
 function transformGroceryList(arrayParameter) {
   let newArray = [];
   arrayParameter.forEach(function(groceryItem) {
-   groceryItem = groceryItem.charAt(0).toUpperCase() + groceryItem.slice(1);
-   newArray.push(groceryItem);
+    groceryItem = groceryItem.charAt(0).toUpperCase() + groceryItem.slice(1);
+    newArray.push(groceryItem);
   })
   return newArray.sort();
 }
+
+//UI logic
+$(document).ready(function() {
+  $("button#addInput").click(function() {
+    $("div#formInputs").append("<input type='text'>");
+  });
+
+  $("form#groceryList").submit(function() {
+    $("#result").empty();
+    event.preventDefault();
+    let groceryList = [];
+    $("div#formInputs > input").each(function() {
+      if($(this).val()){
+        groceryList.push($(this).val());
+      }
+    });
+
+    let transformedList = transformGroceryList(groceryList);
+
+    transformedList.forEach(function(item){
+      $("#result").append("<li>" + item + "</li>");
+    })
+  });
+});
